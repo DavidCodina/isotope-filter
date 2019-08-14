150f5984ede6474ab0a9842f56a08597302dc3c3
David Codina <davidmcodina@hotmail.com>
2019-08-14 15:19:43 -0600

    Removed some comments, and changed animation settings.

--- scripts/isotope.js
+++ scripts/isotope.js
@@ -44,39 +44,8 @@ const filter_functions = {
 
 utlility_functions.addEventListenerByClass('filter-button', 'click', function(event) {
   //Grab the string value associated with the data-filter attribute.
-  let filter_value = event.target.getAttribute('data-filter');
-
-
-  //////////////////////////////////////////////////////////////////////////////
-  //
-  //  Reset the value of filter_value to a filter function if filter_functions[filter_value]
-  //  references a function (i.e., use filter_value as a key to access properties in the
-  //  filter_functions object literal).
-  //  If no such method exists, then it will be undefined.
-  //  In such cases the || will make it so filter_value is still the initial vlue.
-  //
-  //////////////////////////////////////////////////////////////////////////////
-
-  filter_value = filter_functions[ filter_value ] || filter_value;
-
-
-  ////////////////////////////////////////////////////////////////////////////
-  //
-  //  https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
-  //  Not all browsers support forEach on NodeLists.
-  //  You can get around this:
-  //
-  //
-  //        const filter_buttons = document.querySelectorAll('.filter-button');
-  //        [].forEach.call(filter_buttons, function(filter_button) {
-  //          // do whatever
-  //        });
-  //
-  //
-  //  But there's reason why this is hacky:
-  //  https://toddmotto.com/ditch-the-array-foreach-call-nodelist-hack/
-  //
-  ////////////////////////////////////////////////////////////////////////////
+  let filter_value     = event.target.getAttribute('data-filter');
+  filter_value         = filter_functions[ filter_value ] || filter_value;
 
   const filter_buttons = document.getElementsByClassName('filter-button');
     utlility_functions.forEach(filter_buttons, function(filter_button, index) {
@@ -86,27 +55,3 @@ utlility_functions.addEventListenerByClass('filter-button', 'click', function(ev
   this.classList.add('is-checked');
   isotope_container.arrange({ filter: filter_value });
 });
-
-
-//////////////////////////////////////////////////////////////////////////////
-//
-//
-//  The documentation applies an on click event listener to the button container,
-//  and then  only listens for clicks on buttons.
-//  That is an interesting way of doing it, but it looks confusing.
-//
-//      const filter_buttons = document.querySelector('.filter-buttons-container');
-//
-//      filter_buttons.addEventListener('click', function(event) {
-//        if (!matchesSelector(event.target, 'button')) {
-//          console.log("You clicked some empty space.");
-//          return;
-//        }
-//
-//        let filter_value = event.target.getAttribute('data-filter');
-//        filter_value     = filter_functions[ filter_value ] || filter_value;
-//        iso.arrange({ filter: filter_value });
-//      });
-//
-//
-//////////////////////////////////////////////////////////////////////////////
